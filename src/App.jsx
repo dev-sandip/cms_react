283465fbfc2dcae43f2c4f58b83e07df75e4c66d
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-10-18 17:58:31 +0545

    contact page setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,6 @@
+const About = () => {
+  document.title = "About-🚀TextUtils";
+  return <div>About</div>;
+};
+
+export default About;
